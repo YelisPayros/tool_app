82b8cc01b328dbda85d9196f7f1b375ef84e0184
YelisPayros <nicolepayros@gmail.com>
2025-07-02 11:47:40 -0400

    Cambios en el main

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 import 'screens/gender_predictor_screen.dart';
 import 'screens/age_predictor_screen.dart';
+import 'screens/universities_screen.dart';
 import 'screens/weather_screen.dart';
 import 'screens/pokemon_screen.dart';
 import 'screens/wordpress_screen.dart';
-
+import 'screens/about_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -25,9 +26,11 @@ class MyApp extends StatelessWidget {
       '/': (context) => const HomeScreen(),
       '/gender': (context) => const GenderPredictorScreen(),
       '/age': (context) => const AgePredictorScreen(),
+      '/universities': (context) => const UniversitiesScreen(),
       '/weather': (context) => const WeatherScreen(),
       '/pokemon': (context) => const PokemonScreen(),
       '/wordpress': (context) => const WordPressNewsScreen(),
+      '/about': (context) => const AboutScreen(),
     },
     );
   }
